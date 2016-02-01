3b01d6fcdaa014379842b06f901ff39a897cd126
justin01031 <justin010311@hotmail.com>
2016-01-31 17:17:17 -0800

    doing procedure overhead

--- readtime/main.c
+++ readtime/main.c
@@ -38,7 +38,7 @@ static double readtime(int itera){
     
     return average;
 }
-static double loopreadtime(double itera){// not done
+static double loopreadtime(int itera){// not done
    /* uint64_t start;
     uint64_t end;
     uint64_t elapsed;
@@ -71,6 +71,73 @@ static void five(int a, int b,int c, int d, int e){}
 static void six(int a, int b, int c, int d, int e, int f){}
 static void seven(int a, int b, int c, int d, int e, int f, int g){}
 
+static  procedure_overhead(int itera, int para_num){
+    uint64_t start;
+    uint64_t end;
+    uint64_t elapsed;
+    static mach_timebase_info_data_t    sTimebaseInfo;
+    double total_time;
+    if ( sTimebaseInfo.denom == 0 ) {
+        (void) mach_timebase_info(&sTimebaseInfo);
+    }
+    for (int i=0; i<itera; i++) {
+        switch (para_num) {
+            case 0:
+                start=mach_absolute_time();
+                zero();
+                end= mach_absolute_time();
+                break;
+            case 1:
+                start=mach_absolute_time();
+                one(0);
+                end= mach_absolute_time();
+                break;
+            case 2:
+                start=mach_absolute_time();
+                two(0, 0);
+                end= mach_absolute_time();
+                break;
+            case 3:
+                start=mach_absolute_time();
+                three(0, 0, 0);
+                end= mach_absolute_time();
+                break;
+            case 4:
+                start=mach_absolute_time();
+                four(0, 0, 0, 0);
+                end= mach_absolute_time();
+                break;
+            case 5:
+                start=mach_absolute_time();
+                five(0, 0, 0, 0, 0);
+                end= mach_absolute_time();
+                break;
+            case 6:
+                start=mach_absolute_time();
+                six(0, 0, 0, 0, 0, 0);
+                end= mach_absolute_time();
+                break;
+            case 7:
+                start=mach_absolute_time();
+                seven(0, 0, 0, 0, 0, 0, 0);
+                end= mach_absolute_time();
+                break;
+
+            
+            default:
+                start=0;
+                end=0;
+                break;
+        }
+        elapsed = end - start;
+        uint64_t elapsedNano = elapsed * sTimebaseInfo.numer / sTimebaseInfo.denom;
+        total_time += elapsedNano;
+    }
+    double average= total_time/itera;
+    return average;
+    
+    
+}
 int main(int argc, const char * argv[]) {
 
     double a=readtime(10);
